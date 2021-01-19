90f999587b3059692b73ee7a2ed239aef7245a22
Justin Kruger <jdavid.net@gmail.com>
2021-01-18 16:48:25 -0800

    updated setup.py to be a bit more flexible and automated

--- setup.py
+++ setup.py
@@ -1,29 +1,51 @@
-#setup.py
+"""Module Setup."""
 
-from distutils.core import setup
-setup(
-  name = 'photo_downloader',         # How you named your package folder (MyLib)
-  packages = ['photo_downloader'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'a flickr photo downloader',   # Give a short description about your library
-  author = 'Justin Kruger',                   # Type in your name
-  author_email = 'jdavid.net@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Flashback-AI/flickr-photo-downloader',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Flashback-AI/flickr-photo-downloader/archive/main.zip',    # I explain this later on
-  keywords = ['python', 'photos', 'flickr'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'flickrapi',
-          'tinydb',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
-)
+import runpy
+from setuptools import setup, find_packages
+
+# from distutils.core import setup
+
+PACKAGE_NAME = "flickr-photo-downloader"
+version_meta = runpy.run_path("./version.py")
+VERSION = version_meta["__version__"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    line_iter = (line.strip() for line in open(filename))
+    return [line for line in line_iter if line and not line.startswith("#")]
+
+
+if __name__ == "__main__":
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        packages=find_packages(),  # Chose the same as "name"
+        scripts=["scripts/flickr-photo-downloader-cli"],
+        license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+        description='a flickr photo downloader',  # Give a short description about your library
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='Justin Kruger',  # Type in your name
+        author_email='jdavid.net@gmail.com',  # Type in your E-Mail
+        url='https://github.com/Flashback-AI/flickr-photo-downloader',
+        # Provide either the link to your github or to your website
+        download_url='https://github.com/Flashback-AI/flickr-photo-downloader/archive/main.zip',
+        keywords=['python', 'photos', 'flickr'],  # Keywords that define your package best
+        install_requires=parse_requirements("requirements.txt"),
+        python_requires=">=3.7.0",
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            # Choose "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+            'Intended Audience :: Developers',  # Define that your audience are developers
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+        ],
+    )
