cd949a8e14afba490a1f80ab28a7403021fa8977
Justin Kruger <jdavid.net@gmail.com>
2021-01-26 16:18:52 -0800

    fixing linting errors, and trying to get code to pass github action/workflow tests

--- photo_downloader/settings.py
+++ photo_downloader/settings.py
@@ -1,22 +1,25 @@
+"""settings.py"""
 import os
 
 from config import ConfigurationSet, config_from_env, config_from_yaml
 
 
-default_path = "../.config/config.yaml"
-local_path = "../.config-local/config.yaml"
+DEFAULT_PATH = "../.config/config.yaml"
+LOCAL_PATH = "../.config-local/config.yaml"
 
 
 def fix_path(file_path):
+    """fixes a path so project files can be located via a relative path"""
     script_path = os.path.dirname(__file__)
     return os.path.normpath(os.path.join(script_path, file_path))
 
 
 def get_config():
+    """returns the computed deep merge of configurations, from env, yaml"""
     cfg = ConfigurationSet(
         config_from_env("FPD", "__", lowercase_keys=True),
-        config_from_yaml(fix_path(local_path), read_from_file=True),
-        config_from_yaml(fix_path(default_path), read_from_file=True)
+        config_from_yaml(fix_path(LOCAL_PATH), read_from_file=True),
+        config_from_yaml(fix_path(DEFAULT_PATH), read_from_file=True)
     )
 
     print(cfg)
