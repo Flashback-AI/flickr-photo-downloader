297df107bc2524123feb33e242dd419175dc9a93
Justin Kruger <jdavid.net@gmail.com>
2021-02-10 21:08:13 -0800

    it's rough, but i got one small album of 1500 photos to download

--- photo_downloader/agent.py
+++ photo_downloader/agent.py
@@ -2,6 +2,7 @@
 import re
 import types
 import json
+import requests
 # import base64
 # import time
 from datetime import datetime
@@ -10,6 +11,7 @@ from pathlib import Path
 import flickrapi
 from tinydb import TinyDB, Query
 from tinydb.table import Document
+from tinydb.operations import delete
 
 # local libraries
 import settings
@@ -21,18 +23,68 @@ class _Flickr:
     """
     FLICKR_UID_LOC = "8623220@N02"
     FLICKR_SID_LOC_1910S = "72157603624867509"
+    FLICKR_PID_LOC_CAR = '30331622523'
+
     FLICKR_SID_LOC_1930S = "72157603671370361"
     FLICKR_SID_LOC_CIVIL_WAR_FACES = "72157625520211184"
     FLICKR_SID_LOC_PHOTOCHROM_TRAVEL = "72157612249760312"
     FLICKR_SID_LOC_GOTTLIEB_JAZZ = "72157624588645784"
 
+    FLICKR_UID_COSTICĂ_ACSINTE = '109550159@N08'  # "82497773@N00"
+    FLICKR_SID_CA_MILITARI = "72157676896091115"
+    FLICKR_PID_CA_TWO_MEN = '30331622523'
+
     FLICKR_UID_NA = "35740357@N03"
     FLICKR_UID_SMU_LDC = "41131493@N06"
+
     FLICKR_MAX_PAGE_SIZE = 500
 
-    default_user_id = FLICKR_UID_LOC
-    default_album_id = FLICKR_SID_LOC_1910S
-    default_photo_id = '2163445674'
+    default_user_id = FLICKR_UID_COSTICĂ_ACSINTE
+    default_album_id = FLICKR_SID_CA_MILITARI
+    default_photo_id = FLICKR_PID_CA_TWO_MEN
+
+    FLICKR_CDN_URL = 'https://live.staticflickr.com/'
+    FLICKR_PHOTO_URL = 'https://www.flickr.com/photos/'
+    FLICKR_SIZE_SET = [
+        {"suffix": "s", "class": "thumbnail", "label": "Square", "max_edge": 75,
+         "notes": "cropped square"},
+        {"suffix": "q", "class": "thumbnail", "label": "Large Square", "max_edge": 150,
+         "notes": "cropped square"},
+        {"suffix": "t", "class": "thumbnail", "label": "Thumbnail", "max_edge": 100,
+         "notes": None},
+        {"suffix": "m", "class": "small", "label": "Small", "max_edge": 240,
+         "notes": None},
+        {"suffix": "n", "class": "small", "label": "Small 320", "max_edge": 320,
+         "notes": None},
+        {"suffix": "w", "class": "small", "label": "Small 400", "max_edge": 400,
+         "notes": None},
+        {"suffix": "", "class": "medium", "label": "Medium", "max_edge": 500,
+         "notes": None},
+        {"suffix": "z", "class": "medium", "label": "Medium 640", "max_edge": 640,
+         "notes": None},
+        {"suffix": "c", "class": "medium", "label": "Medium 800", "max_edge": 800,
+         "notes": None},
+        {"suffix": "b", "class": "large", "label": "Large", "max_edge": 1024,
+         "notes": None},
+        {"suffix": "h", "class": "large", "label": "Large 1600", "max_edge": 1600,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Large 1600
+        {"suffix": "k", "class": "large", "label": "Large 2048", "max_edge": 2048,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Large 2048
+        {"suffix": "3k", "class": "extra large", "label": "Extra Large", "max_edge": 3072,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Extra Large
+        {"suffix": "4k", "class": "extra large", "label": "Extra Large 4096", "max_edge": 4096,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Extra Large 4096
+        {"suffix": "f", "class": "extra large", "label": "Extra Large Frame", "max_edge": 4096,
+         "notes": "has a unique secret; photo owner can restrict; only exists for 2:1 aspect \
+          ratio photos"},  # TODO: verify Extra Large Frame
+        {"suffix": "5k", "class": "extra large", "label": "Extra Large 5120", "max_edge": 5120,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Extra Large 5120
+        {"suffix": "6k", "class": "extra large", "label": "Extra Large 6144", "max_edge": 6144,
+         "notes": "has a unique secret; photo owner can restrict"},  # TODO: verify Extra Large 6144
+        {"suffix": "o", "class": "original", "label": "Original", "max_edge": None,
+         "notes": "has a unique secret; photo owner can restrict; files have full EXIF data; \
+         files might not be rotate; files can use an arbitrary file extension"},
+    ]
 
     def __init__(self):
         self.cfg = settings.get_config()
@@ -186,11 +238,9 @@ class _Flickr:
         :param func:
         :return:
         """
-        user_id = user_id or self.default_user_id
-        album_id = album_id or self.default_album_id
 
-        set_info = self.get_album_info(user_id, album_id)
-        set_photo_count = set_info['photoset']['count_photos']
+        album_info = self.get_album_info(user_id, album_id)
+        album_photo_count = album_info['photoset']['count_photos']
 
         start = datetime.now()
 
@@ -200,7 +250,7 @@ class _Flickr:
 
             now = datetime.now()
             deltat = now - start
-            percent = count / set_photo_count
+            percent = count / album_photo_count
 
             if percent != 0:
                 remaining = (deltat / percent)
@@ -219,21 +269,87 @@ class _Flickr:
 
             # if (func is not None) and callable(func):  # accepts more callable object types
             if (func is not None) and isinstance(func, types.FunctionType):  # strictly functions
-                func(photo.get('id'), user_id, album_id, photo)
+                func(_photo.get('id'), user_id, album_id, _photo)
+
+    # TODO: DEPRECATE
+    def generate_photo_sizes(self, user_id: str, album_id: str, photo_etree) -> list:
+        """
+        generates a set of possible sizes and urls for the flickr photo
+        :param user_id:
+        :param album_id:
+        :param photo:
+        :return:
+        """
+        _p = {
+            'CDN': self.FLICKR_CDN_URL,
+            'WEB': self.FLICKR_PHOTO_URL,
+            'user_id': user_id,
+            'album_id': album_id,
+            'id': photo_etree.get('id'),
+            'server_id': photo_etree.get('server'),
+            'secret': photo_etree.get('secret')
+        }
+        _sizes = []
+        for s in self.FLICKR_SIZE_SET:
+            # default url format
+            _source = f"{_p['CDN']}{_p['server_id']}/{_p['id']}_{_p['secret']}_{s['suffix']}.jpg"
+
+            # THIS CODE DOESN'T WORK FOR THE FOLLOWING REASONS
+            # All photo image URLs use a secret value that's provided by API methods.
+            # All sizes below the H (1600) size use one shared secret.
+            # All sizes above and including the H (1600) size each use a unique secret
+            # for just themselves.
+            # The original size, regardless of dimensions, always uses its own secret.
+
+            if s['label'] == 'Medium':
+                _source = f"{_p['CDN']}{_p['server_id']}/{_p['id']}_{_p['secret']}.jpg"
+            elif s['label'] == 'Original':
+                # TODO figure out a way to get the format without an api call, /
+                #  jpg is not guaranteed or likely
+                o_format = 'jpg'
+                o_secret = None  # can't get this secret without 2nd api call
+                _source = (
+                    f"{_p['CDN']}{_p['server_id']}/{_p['id']}_{o_secret}_{s['suffix']}."
+                    f"{o_format}"
+                )
+                print(_source)
+                x = requests.head(_source)
+                print(x)
+
+            _photo_size = {
+                'height': None,
+                'width': None,
+                'label': s['label'],
+                'media': 'photo',
+                # photo source image url
+                'source': _source,
+                # photo web page
+                'url': f"{_p['WEB']}{_p['user_id']}/{_p['id']}"
+            }
+
+            _sizes.append(_photo_size)
+
+        return _sizes
 
 
 def _string_to_abv_title(title: str = ''):
     """
-    shortens a string to an abbreviated title
-    ex. 'The Library of Congress -> loc
     :param title:
     :return abv:
     """
-    abv = re.sub(
-        '([Tt]he\\s*)|([a-z]{4,}\\b)|(\\s)',
-        '',
-        title.title().replace('Of', 'O')
-    ).lower()
+    # TODO: make names path safe, exclude (<>:"/\|?*)
+    _title = title
+    abv = _title.title().replace(' ', '').replace('The', '')
+
+    # was getting problematic with international names, and with multiple albums
+    # if len(_title.split(' ')) <= 2 or len(_title <= 16):
+    #     abv = _title.title().replace(' ', '_').lower()
+    # else:
+    #     abv = re.sub(
+    #        '([Tt]he\\s*)|([a-z]{4,}\\b)|(\\s)',
+    #        '',
+    #        _title.title().replace('Of', 'O')
+    #    ).lower()
 
     return abv
 
@@ -262,7 +378,7 @@ class Agent:
 
         self.db = TinyDB(
             settings.fix_path(self.db_path),
-            sort_keys=True,
+            sort_keys=False,
             indent=2,
             separators=(',', ': ')
         )
@@ -306,7 +422,10 @@ class Agent:
         print(json.dumps(license_info, indent=2))
 
         license_q = Query()
-        self.licenses.upsert(license_info, license_q.id == license_info['id'])
+        self.licenses.upsert(
+            license_info,
+            license_q.id == license_info['id']
+        )
 
         return license_info
 
@@ -345,7 +464,10 @@ class Agent:
 
         print(json.dumps(album_info, indent=2))
         album_q = Query()
-        self.albums.upsert(album_info, album_q.id == album_info['id'])
+        self.albums.upsert(
+            album_info,
+            album_q.id == album_info['id']
+        )
 
         return album_info
 
@@ -366,6 +488,18 @@ class Agent:
         :return:
         """
         return self.get_album()['_path']
+
+    def get_album_photos(self) -> list:
+        """
+        queries the DB and returns the set of photos for an album
+        :return:
+        """
+        _q = Query()
+        _photos = self.photos.search(
+            (_q['_user_id'] == self.user_id) &
+            (_q['_album_id'] == self.album_id)
+        )
+        return _photos
 
     def sync_user(self):
         """
@@ -388,7 +522,10 @@ class Agent:
 
         print(json.dumps(user_info, indent=2))
         user_q = Query()
-        self.users.upsert(user_info, user_q.id == user_info['id'])
+        self.users.upsert(
+            user_info,
+            user_q.id == user_info['id']
+        )
 
         return user_info
 
@@ -419,6 +556,9 @@ class Agent:
         photo_exif = self.flickr.get_photo_exif(photo_id)['photo']['exif']
         photo_sizes = self.flickr.get_photo_sizes(photo_id)['sizes']
 
+        photo_info['sizes'] = photo_sizes
+        photo_info['exif'] = photo_exif
+
         # TODO: add _update_date - flickr want's 24hr caching only
         photo_info['_source_api'] = 'flickr'
         photo_info['_type'] = 'photo'
@@ -428,21 +568,32 @@ class Agent:
             photo_info['id']
         ])
 
+        # TODO: technically a photo can belong to two albums,
+        #  so when upserting, we should first check what albums the photos
+        #  is already a part of, and then add this photo to that list
+        photo_info['_album_id'] = self.album_id
+        photo_info['_user_id'] = self.user_id
+
+        # TODO: is this the best photo to get?
+        _source = self.get_photo_largest(photo_id, photo_info)['source']
+        _path = _source.split('/')
+        _ext = _path[-1].split('.')[-1]
+
         photo_info['_path'] = '/'.join([
             _string_to_abv_title(photo_info.get('owner', {}).get('username')),
-            _string_to_abv_title(self.get_album().get('title', {}).get('_content'))
-            # TODO: Add actual file name to path
+            _string_to_abv_title(self.get_album().get('title', {}).get('_content')),
+            '.'.join([photo_info['id'], _ext])
         ])
-
-        photo_info['sizes'] = photo_sizes
-        photo_info['exif'] = photo_exif
 
         print(json.dumps(photo_info, indent=2))
         # print(json.dumps(photo_exif, indent=2))
         # print(json.dumps(photo_sizes, indent=2))
 
         photo_q = Query()
-        self.photos.upsert(photo_info, photo_q.id == photo_info['id'])
+        self.photos.upsert(
+            photo_info,
+            photo_q.id == photo_info['id']
+        )
 
         return photo_info
 
@@ -481,15 +632,20 @@ class Agent:
 
         return _photo_size_info
 
-    def get_photo_largest(self, photo_id: str) -> dict:
+    def get_photo_largest(self, photo_id: str, photo_info=None) -> dict:
         """
         gets the photo, and finds the largest image available
         # TODO is this always the 'Original'?
         :param photo_id:
+        :param photo_info:
         :return:
         """
         _photo = self.get_photo(photo_id)
-        size_list = _photo.get('sizes', {}).get('size', [])
+        if photo_info and photo_info['sizes']['size']:
+            size_list = photo_info.get('sizes', {}).get('size', [])
+        else:
+            size_list = _photo.get('sizes', {}).get('size', [])
+
         _photo_size_info = None
         _largest_area = 0
         _largest_size = None
@@ -515,23 +671,31 @@ class Agent:
         :return:
         """
 
-    def scan_files(self):
+    def scan_files(self, sub_path: str = None):
         """
         scan photo directory for files
         :return:
         """
-        p = Path(self.file_path)
+        _p = self.file_path
+        if sub_path:
+            _p += "/" + sub_path
+
+        p = Path(_p)
         path_glob = p.rglob('*.*')
         file_list = list(path_glob)
 
         print(file_list)
         return file_list
 
-    def scan_photos(self):
+    def scan_photos(self, sub_path: str = None):
         """
         scan photo directory for photos
         :return:
         """
+        _p = self.file_path
+        if sub_path:
+            _p += "/" + sub_path
+
         p = Path(self.file_path)
         path_glob = p.rglob('*.*')
         file_list = []
@@ -548,6 +712,32 @@ class Agent:
         synchronize photo data from flickr
         :return:
         """
+        dir_path = '\\'.join([self.file_path,
+                              self.get_album_path()])
+        p3 = Path(dir_path)
+        if not p3.exists():
+            p3.mkdir(parents=True)
+
+        photo_list = self.get_album_photos()
+        for _photo in photo_list:
+            p = Path(self.file_path + '\\' + _photo['_path'])
+            if p.exists():
+                pass
+            else:
+                _url = self.get_photo_largest(_photo['id'])['source']
+                r = requests.get(_url, allow_redirects=True)
+
+                # TODO: temporary until i fix _path on the photos, wanted photos to have the 'suffix' on them.
+                file_name = re.sub('(_[A-Za-z0-9]*_)', '_', _url).split('/')[-1]
+                file_path = '\\'.join([
+                    self.file_path,
+                    self.get_album_path(),
+                    file_name
+                ])
+                p2 = Path(file_path)
+                p2.write_bytes(r.content)
+                # open(p2, 'wb').write(r.content)
+                pass
 
 
 # print("### Photo Info ###", json.dumps(get_photo_info('2163445674'), indent=2))
@@ -564,9 +754,13 @@ A = Agent(F.default_user_id, F.default_album_id)
 # A.sync_album()
 # A.sync_user()
 # A.sync_licenses()
-photo = A.sync_photo(_Flickr.default_photo_id)
-print(A.get_photo_size(_Flickr.default_photo_id, 'Original')['source'])
-print(A.get_photo_largest(_Flickr.default_photo_id)['source'])
+# photo = A.sync_photo(_Flickr.default_photo_id)
+
+album_path = A.get_album_path()
+A.scan_files(album_path)
+A.scan_photos(album_path)
+A.sync_photos()
+
 
 # print("### License Info ###", json.dumps(
 #     F.get_license_types(), indent=2))
@@ -577,4 +771,22 @@ print(A.get_photo_largest(_Flickr.default_photo_id)['source'])
 #         F.default_album_id
 #     ), indent=2))
 
-# F.walk_album()
+# hack to update photos
+# q = Query()
+# A.photos.upsert({'_album_id': A.album_id}, q.owner.nsid == A.user_id)
+# A.photos.update(delete('_album'), (q.owner.nsid == A.user_id) & (q['_album'].exists()))
+# A.photos.upsert({'_user_id': A.user_id}, q.owner.nsid == A.user_id)
+
+
+def test_func(photo_id, user_id, album_id, photo_etree):
+    F.generate_photo_sizes(user_id, album_id, photo_etree)
+
+
+def _sync_photo(photo_id, user_id, album_id, photo_etree):
+    A.sync_photo(photo_id)
+
+# F.walk_album(
+#     F.default_user_id,
+#     F.default_album_id,
+#     _sync_photo
+# )
